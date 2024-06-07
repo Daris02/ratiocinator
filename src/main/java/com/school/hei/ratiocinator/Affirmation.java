fd7b8144126b898687bc185968e47b6540724ff1
Daris02 <hei.raymond.2@gmail.com>
2024-06-07 15:45:22 +0300

    feat: implement affirmation with test

---
+++ src/main/java/com/school/hei/ratiocinator/Affirmation.java
@@ -0,0 +1,80 @@
+package com.school.hei.ratiocinator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Affirmation {
+    private String contenu;
+    
+    public String calucle() {
+        if (contenu.contains(" Donc ")) {
+            String[] elements = contenu.split(" Donc ");
+            if (elements[0].contains(" et ") || elements[0].contains(" ou ")) {
+                String elem1 = new Affirmation(elements[0]).calucle();
+                String elem2 = new Affirmation(elements[1]).calucle();
+                if (elem1.equals("vrai") && (elem1.equals(elem2))) return "vrai";
+                if (elem1.equals("faux") && (elem1.equals(elem2))) return "faux";
+                if (!elem1.equals(elem2)) return "faux";
+            }
+            if (elements[0].equals("vrai") && elements[1].equals("faux")) return "faux";
+            if (elements[0].equals("vrai") && elements[1].equals("vrai")) return "vrai";
+        }
+
+        if (contenu.contains(" et ")) {
+            String[] elements = contenu.split(" et ");
+            if (verifierAffirmation(elements[0]) != null && 
+                verifierAffirmation(elements[1]) != null && 
+                (verifierAffirmation(elements[0]).equals("vrai") && verifierAffirmation(elements[1]).equals("vrai"))) return "vrai";
+            return "faux";
+        }
+
+        if (contenu.contains(" ou ")) {
+            String[] elements = contenu.split(" ou ");
+            if (verifierAffirmation(elements[0]) != null &&
+                verifierAffirmation(elements[1]) != null &&
+                (verifierAffirmation(elements[0]).equals("vrai") || verifierAffirmation(elements[1]).equals("vrai"))) return "vrai";
+            return "faux";
+        }
+
+        if (verifierAffirmation(contenu) != null && verifierAffirmation(contenu).equals("vrai")) return "vrai";
+        if (verifierAffirmation(contenu) != null && verifierAffirmation(contenu).equals("faux")) return "faux";
+        return "jenesaispas";
+    }
+
+    private String verifierAffirmation(String affirmation) {
+        affirmation = affirmation.replace(".", "");
+        if (recuperRationDesAffirmation().containsKey(affirmation))
+            return recuperRationDesAffirmation().get(affirmation);
+        return null;
+    }
+
+    private static Map<String, String> recuperRationDesAffirmation() {
+        try(BufferedReader lecteur = new BufferedReader(new FileReader("Affirmations.txt"));) {
+            Map<String, String> donnees = new LinkedHashMap<>();
+            String ligne;
+            while ((ligne = lecteur.readLine()) != null) {
+                String[] parties = ligne.split("=");
+                String cle = parties[0].trim();
+                String valeur = parties[1].trim();
+                cle = cle.replace(".", "");
+                valeur = valeur.toLowerCase();
+                donnees.put(cle, valeur);
+            }
+            lecteur.close();
+            return donnees;
+        } catch (IOException e) {
+            System.err.println("Erreur lors de la lecture du fichier : " + e.getMessage());
+        }
+        return null;
+    }
+}
---
+++ src/test/java/ratiocinator/AffirmationTest.java
@@ -0,0 +1,37 @@
+package ratiocinator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.school.hei.ratiocinator.Affirmation;
+
+public class AffirmationTest {
+    
+    @Test
+    void affirmation_vrai() {
+        var veriter_1 = new Affirmation("Lou est beau.");
+        System.out.println(veriter_1.calucle());
+        assertEquals("vrai", veriter_1.calucle());
+
+    }
+
+    @Test
+    void affirmation_mensonge() {
+        var mensonge_1 = new Affirmation("Lou est pauvre.");
+        assertEquals("faux", mensonge_1.calucle());
+    }
+
+    @Test
+    void affirmation_multiple() {
+        var affirmation_1 = new Affirmation("Lou est pauvre et Lou est généreux.");
+        var affirmation_2 = new Affirmation("Lou est beau. Donc Lou est pauvre.");
+        var affirmation_3 = new Affirmation("Lou est pauvre. Donc Lou est généreux.");
+        var affirmation_4 = new Affirmation("Lou est beau ou Lou est généreux. Donc Lou est pauvre.");
+        
+        assertEquals("faux", affirmation_1.calucle());
+        assertEquals("jenesaispas", affirmation_2.calucle());
+        assertEquals("jenesaispas", affirmation_3.calucle());
+        assertEquals("faux", affirmation_4.calucle());
+    }
+}
